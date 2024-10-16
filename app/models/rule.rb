a9f335a3d6502437b02e82443d2ae9102bd27383
edlgld <muramasa3k@gmail.com>
2024-10-16 23:32:50 +0900

    herokuへデプロイ

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,3 +1,7 @@
 class Rule < ApplicationRecord
   belongs_to :team_name
+  has_many :rule_requests, dependent: :destroy
+
+  validates :title, presence: true
+  validates :details, presence: true
 end
--- db/migrate/20241011154113_create_team_names_users.rb
+++ db/migrate/20241011154113_create_team_names_users.rb
@@ -1,6 +1,6 @@
 class CreateTeamNamesUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :team_names_users do |t|
+    create_table :team_names_users, id: :bigint do |t|
       t.references :user, null: false, foreign_key: true
       t.references :team_name, null: false, foreign_key: true
 
